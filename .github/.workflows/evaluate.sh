ad78857b6e045dba953402718aaf3aa955d30fee
Dominik Fuchß <dominik.fuchss@kit.edu>
2021-05-11 11:40:19 +0200

    Added scripts to perform evaluation

--- .github/.workflows/evaluate.sh
+++ .github/.workflows/evaluate.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 ls -l
+
+mkdir results
+
+echo "Evaluation MediaStore"
+python ../eval/eval.py -g ../case_studies/mediastore/gold_standard/Eval_Ecsa_MediaStore.csv -t ./MS -o results/eval_results_MS.csv
+
+echo "Evaluation Teammates"
+python ../eval/eval.py -g ../case_studies/teammates/gold_standard/Goldstandard_Teammates.csv -t ./TM -o results/eval_results_TM.csv
+
+echo "Evaluation TeaStore"
+python ../eval/eval.py -g ../case_studies/teastore/gold_standards/Eval_Ecsa_TeaStore.csv -t ./TS -o results/eval_results_TS.csv
+
+ls -l
--- .github/.workflows/extract-data.sh
+++ .github/.workflows/extract-data.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 ls -l
 
+case_studies="MS TS TM"
+
+for cs in $case_studies; do
+	mkdir $cs
+done
+
+for filename in *.tgz; do
+	name=$(echo $filename | cut -d'.' -f 1)
+	mkdir $name && tar -xzf $filename -C $name --strip-components=1
+	
+	for cs in $case_studies; do
+		if [[ "$name" == *"$cs"* ]]; then
+			mv $name $cs/
+		fi
+	done
+done
+
+ls -l
