5e20c27e1bbc3ea7d5072361b583669e742c99b3
Dominik Fuchß <dominik.fuchss@kit.edu>
2021-05-11 11:50:48 +0200

    Fixed workflow II

--- .github/.workflows/extract-data.sh
+++ .github/.workflows/extract-data.sh
@@ -9,7 +9,8 @@ done
 
 for filename in *.tgz; do
 	name=$(echo $filename | cut -d'.' -f 1)
-	mkdir $name && tar -xzf $filename -C $name --strip-components=1
+	# $filename/$filename because github actions extracts the tgz into a folder called like the tgz
+	mkdir $name && tar -xzf $filename/$filename -C $name --strip-components=1
 	
 	for cs in $case_studies; do
 		if [[ "$name" == *"$cs"* ]]; then
