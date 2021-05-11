020de874df36e247d62eb065cec27a0f023d13cd
Dominik Fuchß <dominik.fuchss@kit.edu>
2021-05-11 10:27:29 +0200

    initial version of zhang16 reproduction CI

--- baselines/zhang16/src/test/java/DocumentTest.java
+++ baselines/zhang16/src/test/java/DocumentTest.java
@@ -1,74 +1,73 @@
-import entity.Document;
-import entity.VerbObjectPhrase;
-import org.junit.Assert;
-import org.junit.Test;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-public class DocumentTest {
+import entity.Document;
+import entity.VerbObjectPhrase;
+
+class DocumentTest {
 
     private Document testDocument = null;
 
-    private Document testDocument(){
+    private Document testDocument() {
 
-        if((this.testDocument == null)){
+        if ((testDocument == null)) {
             Document testDoc = new Document("randomPath");
             String testString = "Tendrils of coldness stretched from its home within her chest, testing her strength before subsiding into stillness once more.";
             List<String> testWords = Arrays.asList(testString.split(" "));
             testDoc.addAllWordsToBag(testWords);
 
-            for(String w: testWords){
+            for (String w : testWords) {
                 testDoc.addPhrase(new VerbObjectPhrase(w, w));
             }
 
-            this.testDocument = testDoc;
+            testDocument = testDoc;
         }
 
-       return this.testDocument;
+        return testDocument;
     }
 
     @Test
-    public void tuningFactorMapCompletenessTest() {
+    void tuningFactorMapCompletenessTest() {
         Document testDocument = testDocument();
 
         int expectedSize = testDocument.getDistinctVOPhrases().size() + testDocument.getDistinctWords().size();
-        Assert.assertEquals(expectedSize, testDocument.getTuningFactors().keySet().size());
+        Assertions.assertEquals(expectedSize, testDocument.getTuningFactors().keySet().size());
 
-        for(String w: testDocument.getBagOfWords()){
-            Assert.assertTrue(testDocument.getTuningFactors().containsKey(w.hashCode()));
+        for (String w : testDocument.getBagOfWords()) {
+            Assertions.assertTrue(testDocument.getTuningFactors().containsKey(w.hashCode()));
         }
 
-        for(VerbObjectPhrase vop: testDocument.getDistinctVOPhrases()){
-            Assert.assertTrue(testDocument.getTuningFactors().containsKey(vop.hashCode()));
+        for (VerbObjectPhrase vop : testDocument.getDistinctVOPhrases()) {
+            Assertions.assertTrue(testDocument.getTuningFactors().containsKey(vop.hashCode()));
         }
     }
 
     @Test
-    public void testUpdateTuningFactor(){
+    void testUpdateTuningFactor() {
         Document testDocument = testDocument();
         VerbObjectPhrase testPhrase = new VerbObjectPhrase("take", "this");
         String testWord = "fish";
 
         testDocument.addWordToBag(testWord);
-        Assert.assertEquals(testDocument.getDefaultWordTuningFactor(), testDocument.getTuningFactors().get(testWord.hashCode()), 0.0);
+        Assertions.assertEquals(testDocument.getDefaultWordTuningFactor(), testDocument.getTuningFactors().get(testWord.hashCode()), 0.0);
 
         testDocument.addWordToBag(testWord, 0.5);
-        Assert.assertEquals(testDocument.getDefaultWordTuningFactor(), testDocument.getTuningFactors().get(testWord.hashCode()), 0.0);
+        Assertions.assertEquals(testDocument.getDefaultWordTuningFactor(), testDocument.getTuningFactors().get(testWord.hashCode()), 0.0);
 
         testDocument.addWordToBag(testWord, 1.1);
-        Assert.assertEquals(1.1, testDocument.getTuningFactors().get(testWord.hashCode()), 0.0);
+        Assertions.assertEquals(1.1, testDocument.getTuningFactors().get(testWord.hashCode()), 0.0);
 
         testDocument.addPhrase(testPhrase);
-        Assert.assertEquals(testDocument.getDefaultVOPhraseTuningFactor(), testDocument.getTuningFactors().get(testPhrase.hashCode()), 0.0);
+        Assertions.assertEquals(testDocument.getDefaultVOPhraseTuningFactor(), testDocument.getTuningFactors().get(testPhrase.hashCode()), 0.0);
 
         testDocument.addPhrase(testPhrase, 0.5);
-        Assert.assertEquals(testDocument.getDefaultVOPhraseTuningFactor(), testDocument.getTuningFactors().get(testPhrase.hashCode()), 0.0);
+        Assertions.assertEquals(testDocument.getDefaultVOPhraseTuningFactor(), testDocument.getTuningFactors().get(testPhrase.hashCode()), 0.0);
 
         testDocument.addPhrase(testPhrase, 5.0);
-        Assert.assertEquals(5.0, testDocument.getTuningFactors().get(testPhrase.hashCode()), 0.0);
+        Assertions.assertEquals(5.0, testDocument.getTuningFactors().get(testPhrase.hashCode()), 0.0);
     }
 
 }
--- baselines/zhang16/src/test/java/ModelProcessorTest.java
+++ baselines/zhang16/src/test/java/ModelProcessorTest.java
@@ -1,19 +1,19 @@
 import java.net.MalformedURLException;
 import java.util.List;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import entity.ModelEntityDocument;
 import process.ModelProcessor;
 import process.NLProcessor;
 
-public class ModelProcessorTest {
+class ModelProcessorTest {
 
     String repoFilePath = "./src/test/resources/test-datasets/TeaStore/model-1/TeaStore_PCM/TeaStore.repository";
     ModelProcessor modelProcessor;
 
-    public ModelProcessor modelProcessor() throws MalformedURLException {
+    ModelProcessor modelProcessor() throws MalformedURLException {
         if (modelProcessor == null) {
             modelProcessor = new ModelProcessor(repoFilePath, new NLProcessor("./src/main/resources/wordnet-database/dict"));
         }
@@ -21,34 +21,34 @@ public class ModelProcessorTest {
     }
 
     @Test
-    public void modelProcessorTest() {
+    void modelProcessorTest() {
         ModelProcessor modelProcessor = null;
         try {
             modelProcessor = modelProcessor();
         } catch (MalformedURLException e) {
             e.printStackTrace();
-            Assert.assertTrue(false);
+            Assertions.assertTrue(false);
             return;
         }
 
-        Assert.assertNotNull(modelProcessor);
-        Assert.assertNotNull(modelProcessor.getParsedRepository());
+        Assertions.assertNotNull(modelProcessor);
+        Assertions.assertNotNull(modelProcessor.getParsedRepository());
 
     }
 
     @Test
-    public void createEntityDocumentsTest() {
+    void createEntityDocumentsTest() {
         List<ModelEntityDocument> entiyDocs = null;
         try {
             entiyDocs = modelProcessor().createModelEntityDocuments();
         } catch (MalformedURLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-            Assert.assertTrue(false);
+            Assertions.assertTrue(false);
             return;
         }
 
-        Assert.assertEquals(entiyDocs.size(), 27);
+        Assertions.assertEquals(entiyDocs.size(), 27);
 
     }
 
